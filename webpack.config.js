b9a79732c9ee3532ad50b99179368e7d3bedc9f0
Laert <nlovnas@mail.ru>
2025-02-26 19:31:29 +0500

    1.24. Начинаем программировать

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const CleanWebpackPlugin = require('webpack-cli');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: 'build', globOptions: { ignore: ['**/index.html'] } },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js'],
+  },
+};
